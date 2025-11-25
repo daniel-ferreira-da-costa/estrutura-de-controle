651b0251e6845d32b537f3ce4022ea5a82133f5a
Daniel Ferreira da Costa <danielferreira@unitins.br>
2025-11-25 15:46:31 -0300

    implementação do menu

--- src/main/java/dev/jaca/Main.java
+++ src/main/java/dev/jaca/Main.java
@@ -1,5 +1,39 @@
 package dev.jaca;
 
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int option = -1;
+        do{
+            System.out.println("\n=== EXERCISE MENU ===");
+            System.out.println("1. Multiplication Table");
+            System.out.println("2. IMC Calculator");
+            System.out.println("3. Even or Odd Checker");
+            System.out.println("4. Remainder");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+            option = scanner.nextInt();
+            switch(option){
+                case 1:
+                    System.out.println("You selected Option 1");
+                    break;
+                case 2:
+                    System.out.println("You selected Option 2");
+                    break;
+                case 3:
+                    System.out.println("You selected Option 3");
+                    break;
+                case 4:
+                    System.out.println("You selected Option 4");
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while(option != 3);
+    }    
 }
